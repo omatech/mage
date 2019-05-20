13344e911660f0273cf8b5c874b9f695c665f549
Christian <cbohollo@omatech.com>
2019-05-20 11:51:48 +0200

    fix public translations, conflic on mage routes

--- src/app/Repositories/Translation/BladeTranslationsGenerator.php
+++ src/app/Repositories/Translation/BladeTranslationsGenerator.php
@@ -36,14 +36,19 @@ class BladeTranslationsGenerator
             window.Translations = $trans;
         </script>";
 
-        if ($debug) {
-            $script .= "<script src=\"/vendor/mage/mage.js\"></script>";
-            $script .= $this->routerGenerator->generate();
-        } else {
-            $script .= "<script type=\"text/javascript\">
-                function normalizeParams(r){if(void 0===r)return{};if(\"object\"!=typeof r)throw new Error(\"Translator Error: Invalid parameters format\");return r}function replaceParams(r,a){return Object.keys(a).forEach(function(n){r=r.replace(\":\"+n,a[n])}),r}window.t=function(r,a=null,n={}){if(null==a)return r;var e=Translations[a];return void 0===e?r:e=replaceParams(e,n=normalizeParams(n))};
-                window.trans=function(a,b={}){if(null==a)return a;var c=Translations[a];return void 0===c?a:(b=normalizeParams(b),c=replaceParams(c),c)};function normalizeParams(a){if(\"undefined\"==typeof a)return{};if(\"object\"!=typeof a)throw new Error(\"Translator Error: Invalid parameters format\");return a}function replaceParams(a,b){return Object.keys(b).forEach(function(c){a=a.replace(\":\"+c,b[c])}),a}
-            </script>";
+        $isMageRoute = config('prefix').'*';
+        $isMageRoute = isRoute($isMageRoute);
+
+        if (!$isMageRoute) {
+            if ($debug) {
+                $script .= "<script src=\"/vendor/mage/mage.js\"></script>";
+                $script .= $this->routerGenerator->generate();
+            } else {
+                $script .= "<script type=\"text/javascript\">
+                    function normalizeParams(r){if(void 0===r)return{};if(\"object\"!=typeof r)throw new Error(\"Translator Error: Invalid parameters format\");return r}function replaceParams(r,a){return Object.keys(a).forEach(function(n){r=r.replace(\":\"+n,a[n])}),r}window.t=function(r,a=null,n={}){if(null==a)return r;var e=Translations[a];return void 0===e?r:e=replaceParams(e,n=normalizeParams(n))};
+                    window.trans=function(a,b={}){if(null==a)return a;var c=Translations[a];return void 0===c?a:(b=normalizeParams(b),c=replaceParams(c),c)};function normalizeParams(a){if(\"undefined\"==typeof a)return{};if(\"object\"!=typeof a)throw new Error(\"Translator Error: Invalid parameters format\");return a}function replaceParams(a,b){return Object.keys(b).forEach(function(c){a=a.replace(\":\"+c,b[c])}),a}
+                </script>";
+            }
         }
 
         return $script;
