f74d62d3a0962393c84b50ba0f801867c32b471a
victorqm98 <45096513+victorqm98@users.noreply.github.com>
2019-10-30 18:31:56 +0100

    interface_create_user (#15)

---
+++ src/app/Contracts/Users/CreateUserInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Omatech\Mage\App\Contracts\Users;
+
+interface CreateUserInterface {
+    public function make($data);
+}
--- src/app/Providers/BindingServiceProvider.php
+++ src/app/Providers/BindingServiceProvider.php
@@ -3,6 +3,8 @@
 namespace Omatech\Mage\App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Omatech\Mage\App\Repositories\User\CreateUser;
+use Omatech\Mage\App\Contracts\Users\CreateUserInterface;
 use Omatech\Mage\App\Repositories\User\UpdateUserAssignRoles;
 use Omatech\Mage\App\Contracts\Users\UpdateUserAssignRolesInterface;
 
@@ -26,5 +28,6 @@ class BindingServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->app->bind(UpdateUserAssignRolesInterface::class, UpdateUserAssignRoles::class);
+        $this->app->bind(CreateUserInterface::class, CreateUser::class);
     }
 }
